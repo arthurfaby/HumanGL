0ae5a04ec1686984d7387ff2b3002fcac850ac3f
afaby <arthurfaby1@gmail.com>
2024-09-26 15:39:47 +0200

    feat(main): create test main that draw random rectangles when typing on up key

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,66 @@
 #include "main.hpp"
 
+#include <iostream>
+
+#include "render/Renderer.hpp"
+
 int main()
 {
     /* Initialisation simple */
-    if (SDL_Init(SDL_INIT_VIDEO) != 0 )
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
-        fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
+        fprintf(stdout, "Échec de l'initialisation de la SDL (%s)\n", SDL_GetError());
         return -1;
     }
     /* Création de la fenêtre */
     SDL_Window* pWindow = NULL;
-    pWindow = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_UNDEFINED,
-                                                              SDL_WINDOWPOS_UNDEFINED,
-                                                              1920,
-                                                              1080,
-                                                              SDL_WINDOW_SHOWN);
+    pWindow = SDL_CreateWindow("HumanGL",SDL_WINDOWPOS_UNDEFINED,
+                               SDL_WINDOWPOS_UNDEFINED,
+                               1920,
+                               1080,
+                               SDL_WINDOW_SHOWN);
 
-    if( pWindow )
+    if (pWindow)
     {
-        SDL_Renderer* renderer = SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_ACCELERATED);
-        while(true)
+        SDL_Renderer* sdlRenderer = SDL_CreateRenderer(
+            pWindow, -1, SDL_RENDERER_ACCELERATED);
+        const auto renderer = Renderer(sdlRenderer);
+        auto color = Color(0, 0, 0);
+        auto point1 = Point2(0, 0);
+        auto point2 = Point2(100, 100);
+        while (true)
         {
             SDL_Event e;
             if (SDL_PollEvent(&e))
             {
+                if (e.type == SDL_KEYDOWN)
+                {
+                    if (e.key.keysym.sym == SDLK_ESCAPE)
+                    {
+                        break;
+                    }
+                    if (e.key.keysym.sym == SDLK_UP)
+                    {
+                        point1.setX(rand() % 1920);
+                        point1.setY(rand() % 1080);
+                        point2.setX(rand() % 1920);
+                        point2.setY(rand() % 1080);
+                    }
+                }
                 if (e.type == SDL_QUIT)
                 {
                     break;
                 }
             }
-            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-            SDL_RenderClear(renderer);
-            SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-            SDL_RenderDrawLine(renderer, 500, 500, 700, 700);
-            SDL_RenderPresent(renderer);
-
-            SDL_Delay(16);
+            renderer.clear(Color(255, 255, 255));;
+            renderer.drawLine(point1, point2, color);
+            renderer.drawEmptyRect(point1, point2, color);
+            renderer.present();
         }
     }
     else
     {
-        fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
+        fprintf(stderr, "Erreur de création de la fenêtre: %s\n", SDL_GetError());
     }
 
     SDL_Quit();
