8789f8ab63e19a4002bf5158293fd1fab1523ac8
afaby <arthurfaby1@gmail.com>
2024-10-07 14:02:36 +0200

    fix(main): remove useless spaces

--- include/main.hpp
+++ include/main.hpp
@@ -1,8 +1,8 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
-# include "SDL2/SDL.h"
-# include <cstdio>
-# include <iostream>
+#include "SDL2/SDL.h"
+#include <cstdio>
+#include <iostream>
 
 #endif //MAIN_HPP
