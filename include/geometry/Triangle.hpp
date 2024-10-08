992ef70ccc4a65f409e6e8d4ccc55c1321685f99
afaby <arthurfaby1@gmail.com>
2024-10-08 11:46:36 +0200

    feat(geometry): create triangle struct

---
+++ include/geometry/Triangle.hpp
@@ -0,0 +1,16 @@
+#ifndef TRIANGLE_HPP
+#define TRIANGLE_HPP
+
+#include "Point2.hpp"
+
+struct Triangle
+{
+    /**
+     * All points of the triangle
+     */
+    Point2 a;
+    Point2 b;
+    Point2 c;
+};
+
+#endif //TRIANGLE_HPP
