a33523a32f4a81cdc3c40870e831fea76f3b8766
Arthur Faby <arthurfaby1@gmail.com>
2024-10-10 15:02:58 +0200

    fix(main): main that draw a polygon

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <ShaderManager.hpp>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "Logger.hpp"
@@ -10,7 +11,7 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
     }
 }
 
-int main(const int argc, char **argv)
+int main(const int argc, char** argv)
 {
     for (int i = 0; i < argc; ++i)
     {
@@ -23,12 +24,13 @@ int main(const int argc, char **argv)
     // Initialize the library
     if (!glfwInit())
     {
+        Logger::error("main,cpp::main(): GLFW initialization failed. Terminating program.");
         return -1;
     }
 
     // Create a windowed mode window and its OpenGL context
     GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
-    if (!window)
+    if (window == nullptr)
     {
         Logger::error("main::glfwCreateWindow(): Window creation failed %s. Terminating GLFW.", window);
         glfwTerminate();
@@ -40,14 +42,63 @@ int main(const int argc, char **argv)
     glfwMakeContextCurrent(window);
     Logger::info("main.cpp::main(): Window context made current.");
 
+    glewExperimental = true;
+    if (glewInit() != GLEW_OK)
+    {
+        Logger::error("main.cpp::main(): GLEW initialization failed. Terminating GLFW.");
+        glfwTerminate();
+        return -1;
+    }
+
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    // @formatter:off
+    const GLfloat polygonVertices[] = {
+        -0.5f, 0.0f, 0.0f,
+        0.0f, 0.0f, 0.0f,
+        -0.5f, -0.25f, 0.0f,
+        -0.5f, -0.25f, 0.0f,
+        0.0f, 0.0f, 0.0f,
+        0.0f, -0.5f, 0.0f,
+        0.0f, -0.5f, 0.0f,
+        0.0f, 0.0f, 0.0f,
+        0.25f, -0.25f, 0.0f,
+        0.25f, -0.25f, 0.0f,
+        0.0f, 0.0f, 0.0f,
+        0.5f, 0.0f, 0.0f
+    };
+    // @formatter:on
+
+    // This will identify our vertex buffer
+    GLuint vertexbuffer;
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    glGenBuffers(1, &vertexbuffer);
+    // The following commands will talk about our 'vertexbuffer' buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    // Give our vertices to OpenGL.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(polygonVertices), polygonVertices, GL_STATIC_DRAW);
+    // This will identify our vertex buffer
+
     glfwSetKeyCallback(window, key_callback);
     Logger::info("main.cpp::main(): Key callback set.");
+
+    // Set the clear color to a grey blue color
+    glClearColor(0.2f, 0.3f, 0.4f, 1.0f);
 
     // Loop until the user closes the window
     while (!glfwWindowShouldClose(window))
     {
         // Render here
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(0, 3,GL_FLOAT,GL_FALSE, 0, (void*) 0);
+
+        glDrawArrays(GL_TRIANGLES, 0, 4 * 3);
+        glDisableVertexAttribArray(0);
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
