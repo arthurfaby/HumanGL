cff091d26dcd8d87662f658ac13ecdb2ce3f6047
Kévin BROUSSE <kbrousse@student.42angouleme.fr>
2024-10-11 15:25:44 +0200

    feat(Matrix): Created Matrix4 class

--- include/maths/matrices/Matrix4.hpp
+++ include/maths/matrices/Matrix4.hpp
@@ -1,56 +1,56 @@
 #ifndef MATRIX4_HPP
 #define MATRIX4_HPP
+#include <array>
+#include <Vector4.hpp>
 
 class Matrix4
 {
 public:
     // Constructors
     Matrix4() = default;
-    explicit Matrix4(const float[16]);
+    explicit Matrix4(const std::array<float, 16>&);
     Matrix4(const Matrix4& other) = default;
     
     // Destructor
     ~Matrix4() = default;
     
     // Getters
-    [[nodiscard]] int getM() const;
-    [[nodiscard]] int getN() const ;
-    [[nodiscard]] float getValueAt(int, int) const;
-    [[nodiscard]] float getMatrix(int) const;
-
-    // Setters
-    void setRow(int, float[4]);
-    void setCol(int, float[4]);
-    void setValueAt(int, int, float);
-    void setMatrix(float[16]);
+    [[nodiscard]] const float* getData() const;
+    [[nodiscard]] Vector4 getRowOne() const;
+    [[nodiscard]] Vector4 getRowTwo() const;
+    [[nodiscard]] Vector4 getRowThree() const;
+    [[nodiscard]] Vector4 getRowFour() const;
+    [[nodiscard]] Vector4 getColumnOne() const;
+    [[nodiscard]] Vector4 getColumnTwo() const;
+    [[nodiscard]] Vector4 getColumnThree() const;
+    [[nodiscard]] Vector4 getColumnFour() const;
 
     // Operator overloads
-    Matrix4& operator=(const Matrix4& other) = default;
+    Matrix4& operator=(const Matrix4& other);
+    Matrix4 operator*(const Matrix4& other) const;
+
+    Vector4 operator*(const Vector4& other) const;
+
+    [[nodiscard]] std::string toString() const;
 
     // Methods
-    
+    static Matrix4 createTranslationMatrix(float, float, float);
+    static Matrix4 createRotationXMatrix(double);
+    static Matrix4 createRotationYMatrix(double);
+    static Matrix4 createRotationZMatrix(double);
+    static Matrix4 createScalingMatrix(float, float, float);
+
 private:
     /**
-     * The data of the matrix such as :<br>
-     *  [0, 1, 2, 3]<br>
-     *  [4, 5, 6, 7]<br>
-     *  [8, 9, 10, 11]<br>
-     *  [12, 13, 14, 15]<br>
-     */
+    * The data of the matrix such as :<br>
+    *  [0, 1, 2, 3]<br>
+    *  [4, 5, 6, 7]<br>
+    *  [8, 9, 10, 11]<br>
+    *  [12, 13, 14, 15]<br>
+    */
     float _data[16]{};
-
-    /**
-    * The number of rows of the matrix.
-    */
-    int _m = 4;
-
-    /**
-    * The number of columns of the matrix.
-    */
-    int _n = 4;
-
-    // Methods
-    
 };
+
+std::ostream& operator<<(std::ostream& os, const Matrix4&);
 
 #endif //MATRIX4_HPP
--- src/maths/matrices/Matrix4.cpp
+++ src/maths/matrices/Matrix4.cpp
@@ -1,60 +1,351 @@
-#include "Matrix4.hpp"
-#include <stdexcept>
+#include <array>
+#include <cmath>
+#include <Matrix4.hpp>
+#include <sstream>
+#include <vector>
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Constructors
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /**
- * Construct a new Matrix4 object.
+ * Construct a new Matrix4 object using an array of 16 float values.
  *
- * @param newData An array of 16 float values to initialize the matrix with
+ * @param list An array of 16 float values to initialize the matrix with
  */
-Matrix4::Matrix4(const float newData[16])
+Matrix4::Matrix4(const std::array<float, 16>& list)
 {
     for (int i = 0; i < 16; ++i)
     {
-        _data[i] = newData[i];
+        _data[i] = list[i];
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Getters
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 /**
- * @return The number of rows of the matrix
+ * @return The data of the matrix
  */
-[[nodiscard]] int Matrix4::getM() const
+[[nodiscard]] const float* Matrix4::getData() const
 {
-    return _m;
+    return _data;
 }
 
 /**
- * @return The number of columns of the matrix
+ * @return The first row of the matrix
  */
-[[nodiscard]] int Matrix4::getN() const
+[[nodiscard]] Vector4 Matrix4::getRowOne() const
 {
-    return _n;
+    return Vector4(_data[0], _data[1], _data[2], _data[3]);
 }
 
 /**
- * Get the value at the specified row and column of the matrix.
- *
- * @param m The row of the matrix to access between 1 and 4
- * @param n The column of the matrix to access between 1 and 4
- *
- * @return The value at [m, n] in the matrix
+ * @return The second row of the matrix
  */
-[[nodiscard]] float Matrix4::getValueAt(int m, int n) const
+[[nodiscard]] Vector4 Matrix4::getRowTwo() const
 {
-    m -= 1;
-    n -= 1;
+    return Vector4(_data[4], _data[5], _data[6], _data[7]);
+}
 
-    if ((m < 0 || m > 3) || (n < 0 || n > 3))
+/**
+ * @return The third row of the matrix
+ */
+[[nodiscard]] Vector4 Matrix4::getRowThree() const
+{
+    return Vector4(_data[8], _data[9], _data[10], _data[11]);
+}
+
+/**
+ * @return The fourth row of the matrix
+ */
+[[nodiscard]] Vector4 Matrix4::getRowFour() const
+{
+    return Vector4(_data[12], _data[13], _data[14], _data[15]);
+}
+
+/**
+ * @return The first column of the matrix
+ */
+[[nodiscard]] Vector4 Matrix4::getColumnOne() const
+{
+    return Vector4(_data[0], _data[4], _data[8], _data[12]);
+}
+
+/**
+ * @return The second column of the matrix
+ */
+[[nodiscard]] Vector4 Matrix4::getColumnTwo() const
+{
+    return Vector4(_data[1], _data[5], _data[9], _data[13]);
+}
+
+/**
+ * @return The third column of the matrix
+ */
+[[nodiscard]] Vector4 Matrix4::getColumnThree() const
+{
+    return Vector4(_data[2], _data[6], _data[10], _data[14]);
+}
+
+/**
+ * @return The fourth column of the matrix
+ */
+[[nodiscard]] Vector4 Matrix4::getColumnFour() const
+{
+    return Vector4(_data[3], _data[7], _data[11], _data[15]);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Operator overloads
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Assignation operator overload.<br>
+ * Assign the values of the other matrix to this matrix.
+ *
+ * @param other The matrix to assign
+ *
+ * @return itself
+ */
+Matrix4& Matrix4::operator=(const Matrix4& other)
+{
+    if (this != &other)
     {
-        throw std::out_of_range("Matrix index out of range");
+        const float* otherData = other.getData();
+
+        for (int i = 0; i < 16; ++i)
+        {
+            _data[i] = otherData[i];
+        }
     }
-    return _data[m * 4 + n];
+    return *this;
 }
 
-[[nodiscard]] float Matrix4::getMatrix(int) const
+/**
+ * Multiplication operator overload.<br>
+ * Create a new Matrix4 initialized with the product of the left and right matrices.
+ *
+ * @param other The matrix to multiply by
+ *
+ * @return A copy of the created Matrix4
+ */
+Matrix4 Matrix4::operator*(const Matrix4& other) const
 {
-    return 0.0f;
+    const std::array rows = {getRowOne(), getRowTwo(), getRowThree(), getRowFour()};
+    const std::array columns = {
+        other.getColumnOne(),
+        other.getColumnTwo(),
+        other.getColumnThree(),
+        other.getColumnFour()
+    };
+    std::vector<float> resVector;
+
+    for (const Vector4& row: rows)
+    {
+        for (const Vector4& col: columns)
+        {
+            resVector.push_back(row[0] * col[0] + row[1] * col[1] + row[2] * col[2] + row[3] * col[3]);
+        }
+    }
+
+    // Convert the std::vector<float> to std::array<float, 16>
+    std::array<float, 16> resultArray{};
+    std::copy(resVector.begin(), resVector.end(), resultArray.begin());
+
+    return Matrix4(resultArray);
+}
+
+/**
+ * Multiplication operator overload.<br>
+ * Create a new Vector4 initialized with the product of the left matrix by the right vector.
+ *
+ * @param other The vector to multiply
+ *
+ * @return A copy of the created Vector4
+ */
+Vector4 Matrix4::operator*(const Vector4& other) const
+{
+    const float otherX = other.getX();
+    const float otherY = other.getY();
+    const float otherZ = other.getZ();
+    const float otherW = other.getW();
+
+    const float x = _data[0] * otherX + _data[1] * otherY + _data[2] * otherZ + _data[3] * otherW;
+    const float y = _data[4] * otherX + _data[5] * otherY + _data[6] * otherZ + _data[7] * otherW;
+    const float z = _data[8] * otherX + _data[9] * otherY + _data[10] * otherZ + _data[11] * otherW;
+    const float w = _data[12] * otherX + _data[13] * otherY + _data[14] * otherZ + _data[15] * otherW;
+
+    return Vector4(x, y, z, w);
+}
+
+/**
+ * Output stream operator overload.<br>
+ * Print the matrix to the output stream.
+ *
+ * @param os The output stream
+ * @param matrix The matrix to print
+ *
+ * @return The output stream
+ */
+std::ostream& operator<<(std::ostream& os, const Matrix4& matrix)
+{
+    const auto matrixData = matrix.getData();
+    os << "Matrix4("
+            << "Row1: [" << matrixData[0] << ", " << matrixData[1] << ", " << matrixData[2] << ", " << matrixData[3] <<
+            "], "
+            << "Row2: [" << matrixData[4] << ", " << matrixData[5] << ", " << matrixData[6] << ", " << matrixData[7] <<
+            "], "
+            << "Row3: [" << matrixData[8] << ", " << matrixData[9] << ", " << matrixData[10] << ", " << matrixData[11]
+            << "], "
+            << "Row4: [" << matrixData[12] << ", " << matrixData[13] << ", " << matrixData[14] << ", " << matrixData[15]
+            << "])";
+    return os;
+}
+
+/**
+ * @return A string containing the data of the matrix
+ */
+[[nodiscard]] std::string Matrix4::toString() const
+{
+    std::ostringstream oss;
+    oss << "Matrix4("
+            << "Row1: [" << _data[0] << ", " << _data[1] << ", " << _data[2] << ", " << _data[3] << "], "
+            << "Row2: [" << _data[4] << ", " << _data[5] << ", " << _data[6] << ", " << _data[7] << "], "
+            << "Row3: [" << _data[8] << ", " << _data[9] << ", " << _data[10] << ", " << _data[11] << "], "
+            << "Row4: [" << _data[12] << ", " << _data[13] << ", " << _data[14] << ", " << _data[15] << "])";
+    return oss.str();
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Methods
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Create a translation matrix such as :<br>
+ *  [1, 0, 0, tx]<br>
+ *  [0, 1, 0, ty]<br>
+ *  [0, 0, 1, tz]<br>
+ *  [0, 0, 0, 1 ]<br>
+ *
+ * @param tx The x translation
+ * @param ty The y translation
+ * @param tz The z translation
+ *
+ * @return The created translation matrix
+ */
+Matrix4 Matrix4::createTranslationMatrix(const float tx, const float ty, const float tz)
+{
+    //@formatter:off
+    return Matrix4({
+        1, 0, 0, tx,
+        0, 1, 0, ty,
+        0, 0, 1, tz,
+        0, 0, 0, 1,
+    });
+    //@formatter:on
+}
+
+/**
+ * Create a rotation matrix for the X axis such as :<br>
+ *  [1, 0, 0, 0]<br>
+ *  [0, cos(angle), -sin(angle), 0]<br>
+ *  [0, sin(angle), cos(angle), 0]<br>
+ *  [0, 0, 0, 1 ]<br>
+ *
+ * @param angleD The angle in degrees of type double
+ *
+ * @return The created translation matrix
+ */
+Matrix4 Matrix4::createRotationXMatrix(const double angleD)
+{
+    const auto cosAngleF = static_cast<float>(cos(angleD));
+    const auto sinAngleF = static_cast<float>(sin(angleD));
+
+    //@formatter:off
+    return Matrix4({
+        1,         0,          0, 0,
+        0, cosAngleF, -sinAngleF, 0,
+        0, sinAngleF,  cosAngleF, 0,
+        0,         0,          0, 1
+    });
+    //@formatter:on
+}
+
+/**
+ * Create a rotation matrix for the Y axis such as :<br>
+ *  [cos(angle), 0, sin(angle), 0]<br>
+ *  [0, 1, 0, 0]<br>
+ *  [-sin(angle), 0, cos(angle), 0]<br>
+ *  [0, 0, 0, 1 ]<br>
+ *
+ * @param angleD The angle in degrees of type double
+ *
+ * @return The created translation matrix
+ */
+Matrix4 Matrix4::createRotationYMatrix(const double angleD)
+{
+    const auto cosAngleF = static_cast<float>(cos(angleD));
+    const auto sinAngleF = static_cast<float>(sin(angleD));
+
+    //@formatter:off
+    return Matrix4({
+         cosAngleF, 0, sinAngleF, 0,
+                 0, 1,         0, 0,
+        -sinAngleF, 0, cosAngleF, 0,
+                 0, 0,         0, 1
+    });
+    //@formatter:on
+}
+
+/**
+ * Create a rotation matrix for the Z axis such as :<br>
+ *  [cos(angle), -sin(angle), 0, 0]<br>
+ *  [sin(angle), cos(angle), 0, 0]<br>
+ *  [0, 0, 1, 0]<br>
+ *  [0, 0, 0, 1]<br>
+ *
+ * @param angleD The angle in degrees of type double
+ *
+ * @return The created translation matrix
+ */
+Matrix4 Matrix4::createRotationZMatrix(const double angleD)
+{
+    const auto cosAngleF = static_cast<float>(cos(angleD));
+    const auto sinAngleF = static_cast<float>(sin(angleD));
+
+    //@formatter:off
+    return Matrix4({
+        cosAngleF, -sinAngleF, 0, 0,
+        sinAngleF,  cosAngleF, 0, 0,
+                0,          0, 1, 0,
+                0,          0, 0, 1
+    });
+    //@formatter:on
+}
+
+/**
+ * Create a scaling matrix such as :<br>
+ *  [sx, 0, 0, 0]<br>
+ *  [0, sy, 0, 0]<br>
+ *  [0, 0, sz, 0]<br>
+ *  [0, 0, 0, 1 ]<br>
+ *
+ * @param sx The x translation
+ * @param sy The y translation
+ * @param sz The z translation
+ *
+ * @return The created translation matrix
+ */
+Matrix4 Matrix4::createScalingMatrix(const float sx, const float sy, const float sz)
+{
+    //@formatter:off
+    return Matrix4({
+        sx,  0,  0, 0,
+         0, sy,  0, 0,
+         0,  0, sz, 0,
+         0,  0,  0, 1
+    });
+    //@formatter:on
 }
