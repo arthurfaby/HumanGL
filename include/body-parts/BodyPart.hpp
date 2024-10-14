bb2e1e6ac51c7a2a755c7697867dcfcace04d2e3
Arthur Faby <arthurfaby1@gmail.com>
2024-10-14 14:49:54 +0200

    feat(bodyparts): add cube that can rotate and translate

--- include/body-parts/BodyPart.hpp
+++ include/body-parts/BodyPart.hpp
@@ -7,38 +7,93 @@
 class BodyPart
 {
 public:
-    // Constructor
-    BodyPart() = delete;
-    BodyPart(const BodyPart& other) = delete;
+  // Constructor
+  BodyPart() = delete;
+  explicit BodyPart(const Vector4& position);
+  BodyPart(const BodyPart& other) = delete;
 
-    // Destructor
-    ~BodyPart() = delete;
+  // Destructor
+  virtual ~BodyPart() = default;
 
-    // Getters
-    [[nodiscard]] Vector4 getPosition() const;
-    [[nodiscard]] std::vector<BodyPart> getChildren() const;
+  // Getters
+  [[nodiscard]] Vector4 getPosition() const;
+  [[nodiscard]] Vector4 getDir() const;
+  [[nodiscard]] std::vector<BodyPart*> getChildren() const;
 
-    // Setters
-    void setPosition(const Vector4& position);
+  // Setters
+  void setPosition(const Vector4& position);
+  void setDir(const Vector4& dir);
 
-    // Operator overloads
-    BodyPart& operator=(const BodyPart& other) = delete;
+  // Operator overloads
+  BodyPart& operator=(const BodyPart& other) = delete;
 
-    // Methods
-    void draw() const;
-    void addChild(const BodyPart& child);
-    void removeChild(const BodyPart& child);
+  // Methods
+  void addChild(BodyPart* child);
+  void removeChild(BodyPart* child);
 
-private:
-    /**
-      * The children of the body part.
-      */
-    std::vector<BodyPart> _children;
+protected:
+  /**
+    * The lines vertices of the body part.
+    */
+  std::vector<float> _linesVertices;
 
-    /**
-      * The position of the body part.
-      */
-    Vector4 _position;
+  /**
+    * The lines colors of the body part.
+    */
+  std::vector<float> _linesColors;
+
+  /**
+    * The triangles vertices of the body part.
+    */
+  std::vector<float> _trianglesVertices;
+
+  /**
+    * The triangles colors of the body part.
+    */
+  std::vector<float> _trianglesColors;
+
+  /**
+    * The start index of the lines vertices buffer.
+    */
+  unsigned int _startLinesVerticesBufferStartIndex{};
+
+  /**
+    * The start index of the lines colors buffer.
+    */
+  unsigned int _startLinesColorBufferStartIndex{};
+
+  /**
+    * The start index of the triangles vertices buffer.
+    */
+  unsigned int _startTrianglesVerticesBufferStartIndex{};
+
+  /**
+    * The start index of the triangles colors buffer.
+    */
+  unsigned int _startTrianglesColorBufferStartIndex{};
+
+  /**
+    * The children of the body part.
+    */
+  std::vector<BodyPart*> _children;
+
+  /**
+    * The parent of the body part.
+    */
+  BodyPart* _parent = nullptr;
+
+  /**
+    * The position of the body part.
+    */
+  Vector4 _position;
+
+  /**
+    * The direction of the body part.
+    */
+  Vector4 _dir;
+
+  // Methods
+  void _updateVertices(bool modifyBuffer = true);
 };
 
 #endif //BODY_PART_HPP
--- src/body-parts/BodyPart.cpp
+++ src/body-parts/BodyPart.cpp
@@ -1,11 +1,34 @@
-#include "BodyPart.hpp"
+#include <BodyPart.hpp>
+#include <BufferManager.hpp>
+#include <cmath>
+#include <Matrix4.hpp>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Constructors
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Constructor.
+ *
+ * @param position The position of the body part
+ */
+BodyPart::BodyPart(const Vector4& position)
+{
+    _position = position;
+
+    _startLinesVerticesBufferStartIndex = BufferManager::addLinesVertices(_linesVertices);
+    _startLinesColorBufferStartIndex = BufferManager::addLinesColors(_linesColors);
+    _startTrianglesVerticesBufferStartIndex = BufferManager::addTrianglesVertices(_trianglesVertices);
+    _startTrianglesColorBufferStartIndex = BufferManager::addTrianglesColors(_trianglesColors);
+    _updateVertices(false);
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Getters
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /**
- * @return The position of the body part.
+ * @return The position of the body part
  */
 [[nodiscard]] Vector4 BodyPart::getPosition() const
 {
@@ -13,9 +36,17 @@
 }
 
 /**
- * @return The children of the body part.
+ * @return The direction of the body part
  */
-[[nodiscard]] std::vector<BodyPart> BodyPart::getChildren() const
+[[nodiscard]] Vector4 BodyPart::getDir() const
+{
+    return _dir;
+}
+
+/**
+ * @return The children of the body part
+ */
+[[nodiscard]] std::vector<BodyPart*> BodyPart::getChildren() const
 {
     return _children;
 }
@@ -32,6 +63,21 @@
 void BodyPart::setPosition(const Vector4& position)
 {
     _position = position;
+    _updateVertices();
+}
+
+/**
+ * Set the direction of the body part.
+ *
+ * @param dir The new position of the body part.
+ */
+void BodyPart::setDir(const Vector4& dir)
+{
+    constexpr float pi = std::numbers::pi;
+    _dir.setX(fmodf(dir.getX() + pi, 2 * pi) - pi);
+    _dir.setY(fmodf(dir.getY() + pi, 2 * pi) - pi);
+    _dir.setZ(fmodf(dir.getZ() + pi, 2 * pi) - pi);
+    _updateVertices();
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -39,19 +85,11 @@ void BodyPart::setPosition(const Vector4& position)
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /**
- * Draw the body part.
- */
-void BodyPart::draw() const
-{
-    // Draw the body part
-}
-
-/**
  * Add a child to the body part.
  *
- * @param child The child to add.
+ * @param child The child to add
  */
-void BodyPart::addChild(const BodyPart& child)
+void BodyPart::addChild(BodyPart* child)
 {
     _children.push_back(child);
 }
@@ -59,9 +97,137 @@ void BodyPart::addChild(const BodyPart& child)
 /**
  * Remove a child from the body part.
  *
- * @param child The child to remove.
+ * @param child The child to remove
  */
-void BodyPart::removeChild(const BodyPart& child)
+void BodyPart::removeChild(BodyPart* child)
 {
     std::erase(_children, child);
+}
+
+/**
+ * Update the vertices of the body part.
+ *
+ * @param modifyBuffer If true, the buffer will be modified
+ */
+void BodyPart::_updateVertices(bool modifyBuffer /*= true*/)
+{
+    float cubeSize = 0.15f;
+    Matrix4 rotationMatrix = Matrix4::createRotationMatrix(_dir.getX(), _dir.getY(), _dir.getZ());
+
+    Vector4 frontTopLeft = Vector4(-cubeSize, cubeSize, cubeSize, 1.0f);
+    Vector4 frontTopRight = Vector4(cubeSize, cubeSize, cubeSize, 1.0f);
+    Vector4 frontBottomLeft = Vector4(-cubeSize, -cubeSize, cubeSize, 1.0f);
+    Vector4 frontBottomRight = Vector4(cubeSize, -cubeSize, cubeSize, 1.0f);
+
+    Vector4 backTopLeft = Vector4(-cubeSize, cubeSize, -cubeSize, 1.0f);
+    Vector4 backTopRight = Vector4(cubeSize, cubeSize, -cubeSize, 1.0f);
+    Vector4 backBottomLeft = Vector4(-cubeSize, -cubeSize, -cubeSize, 1.0f);
+    Vector4 backBottomRight = Vector4(cubeSize, -cubeSize, -cubeSize, 1.0f);
+
+    frontTopLeft = rotationMatrix * frontTopLeft + _position;
+    frontTopRight = rotationMatrix * frontTopRight + _position;
+    frontBottomLeft = rotationMatrix * frontBottomLeft + _position;
+    frontBottomRight = rotationMatrix * frontBottomRight + _position;
+
+    backTopLeft = rotationMatrix * backTopLeft + _position;
+    backTopRight = rotationMatrix * backTopRight + _position;
+    backBottomLeft = rotationMatrix * backBottomLeft + _position;
+    backBottomRight = rotationMatrix * backBottomRight + _position;
+
+    //@formatter:off
+    _trianglesVertices = {
+        frontTopLeft.getX(), frontTopLeft.getY(), frontTopLeft.getZ(),
+        frontBottomLeft.getX(), frontBottomLeft.getY(), frontBottomLeft.getZ(),
+        frontBottomRight.getX(), frontBottomRight.getY(), frontBottomRight.getZ(),
+        frontTopLeft.getX(), frontTopLeft.getY(), frontTopLeft.getZ(),
+        frontTopRight.getX(), frontTopRight.getY(), frontTopRight.getZ(),
+        frontBottomRight.getX(), frontBottomRight.getY(), frontBottomRight.getZ(),
+
+        backTopLeft.getX(), backTopLeft.getY(), backTopLeft.getZ(),
+        backBottomLeft.getX(), backBottomLeft.getY(), backBottomLeft.getZ(),
+        backBottomRight.getX(), backBottomRight.getY(), backBottomRight.getZ(),
+        backTopLeft.getX(), backTopLeft.getY(), backTopLeft.getZ(),
+        backTopRight.getX(), backTopRight.getY(), backTopRight.getZ(),
+        backBottomRight.getX(), backBottomRight.getY(), backBottomRight.getZ(),
+
+        backTopLeft.getX(), backTopLeft.getY(), backTopLeft.getZ(),
+        backBottomLeft.getX(), backBottomLeft.getY(), backBottomLeft.getZ(),
+        frontBottomLeft.getX(), frontBottomLeft.getY(), frontBottomLeft.getZ(),
+        backTopLeft.getX(), backTopLeft.getY(), backTopLeft.getZ(),
+        frontTopLeft.getX(), frontTopLeft.getY(), frontTopLeft.getZ(),
+        frontBottomLeft.getX(), frontBottomLeft.getY(), frontBottomLeft.getZ(),
+
+        frontTopRight.getX(), frontTopRight.getY(), frontTopRight.getZ(),
+        frontBottomRight.getX(), frontBottomRight.getY(), frontBottomRight.getZ(),
+        backBottomRight.getX(), backBottomRight.getY(), backBottomRight.getZ(),
+        frontTopRight.getX(), frontTopRight.getY(), frontTopRight.getZ(),
+        backTopRight.getX(), backTopRight.getY(), backTopRight.getZ(),
+        backBottomRight.getX(), backBottomRight.getY(), backBottomRight.getZ(),
+
+        frontTopLeft.getX(), frontTopLeft.getY(), frontTopLeft.getZ(),
+        backTopLeft.getX(), backTopLeft.getY(), backTopLeft.getZ(),
+        backTopRight.getX(), backTopRight.getY(), backTopRight.getZ(),
+        frontTopLeft.getX(), frontTopLeft.getY(), frontTopLeft.getZ(),
+        frontTopRight.getX(), frontTopRight.getY(), frontTopRight.getZ(),
+        backTopRight.getX(), backTopRight.getY(), backTopRight.getZ(),
+
+        frontBottomLeft.getX(), frontBottomLeft.getY(), frontBottomLeft.getZ(),
+        backBottomLeft.getX(), backBottomLeft.getY(), backBottomLeft.getZ(),
+        backBottomRight.getX(), backBottomRight.getY(), backBottomRight.getZ(),
+        frontBottomLeft.getX(), frontBottomLeft.getY(), frontBottomLeft.getZ(),
+        frontBottomRight.getX(), frontBottomRight.getY(), frontBottomRight.getZ(),
+        backBottomRight.getX(), backBottomRight.getY(), backBottomRight.getZ()
+    };
+
+    _trianglesColors = {
+        1.0f, 0.0f, 0.0f,
+        1.0f, 0.0f, 0.0f,
+        1.0f, 0.0f, 0.0f,
+        1.0f, 0.0f, 0.0f,
+        1.0f, 0.0f, 0.0f,
+        1.0f, 0.0f, 0.0f,
+
+        0.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+
+        0.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f,
+
+        1.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 0.0f,
+
+        1.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 1.0f,
+
+        0.0f, 1.0f, 1.0f,
+        0.0f, 1.0f, 1.0f,
+        0.0f, 1.0f, 1.0f,
+        0.0f, 1.0f, 1.0f,
+        0.0f, 1.0f, 1.0f,
+        0.0f, 1.0f, 1.0f
+    };
+    //@formatter:on
+
+    _startTrianglesVerticesBufferStartIndex = BufferManager::modifyTrianglesVertices(
+        _startTrianglesVerticesBufferStartIndex,
+        _trianglesVertices);
+    _startTrianglesColorBufferStartIndex = BufferManager::modifyTrianglesColors(
+        _startTrianglesColorBufferStartIndex,
+        _trianglesColors);
 }
