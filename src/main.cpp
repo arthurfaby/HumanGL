758b6141d3f3ec35fdf225cc3952206aa65b124f
Kévin BROUSSE <kbrousse@student.42angouleme.fr>
2024-11-13 17:51:12 +0100

    feat(main): Updated main with good matrices to send to the vertex shader

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <BodyPart.hpp>
 #include <BufferManager.hpp>
+#include <Camera.hpp>
 #include <cmath>
 #include <Matrix4.hpp>
 #include <ShaderManager.hpp>
@@ -7,20 +8,9 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "Logger.hpp"
+#include "WindowDefines.hpp"
 
-#define ToRadian(x) ((x) * (M_PI / 180.0))
-
-#define WINDOW_WIDTH 1024.0f                        // Window width in pixels
-#define WINDOW_HEIGHT 576.0f                        // Window height in pixels
-#define ASPECT_RATIO (WINDOW_WIDTH / WINDOW_HEIGHT) // Aspect ratio of the window
-#define FPS_LIMIT 60                                // Frames per second limit
-#define NEAR_PLANE_Z 0.1f                           // Near clipping plane
-#define FAR_PLANE_Z 100.0f                          // Far clipping plane
-#define Z_RANGE (NEAR_PLANE_Z - FAR_PLANE_Z)        // Range of the z-axis
-
-Vector4 cameraPos(0.0f, 0.0f, 0.0f, 1.0f);
-
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+static void key_callback(GLFWwindow* window, const int key, int scancode, const int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     {
@@ -32,38 +22,15 @@ void render(GLFWwindow* window, const double& now, double& lastRenderTime, unsig
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    constexpr float fov = 90.0f;
-    constexpr float halfFov = fov / 2;
-    constexpr float radHalfFov = ToRadian(halfFov);
-    const float tanRadHalfFov = tanf(radHalfFov);
-    const float inverseTanRadHalfFov = 1 / tanRadHalfFov;
+    const Matrix4 allTranslationMatrices = Matrix4::createTranslationMatrix(
+                                               Camera::getInstance().getPosition().getX(),
+                                               Camera::getInstance().getPosition().getY(),
+                                               Camera::getInstance().getPosition().getZ());
 
-    // @formatter:off
-    const Matrix4 matrixProjection({
-        inverseTanRadHalfFov / ASPECT_RATIO, 0, 0, 0,
-        0, inverseTanRadHalfFov, 0, 0,
-        0, 0, (-FAR_PLANE_Z - NEAR_PLANE_Z)/Z_RANGE, (2 * FAR_PLANE_Z * NEAR_PLANE_Z)/Z_RANGE,
-        0, 0, 1, 0,
-    });
-    // @formatter:on
+    const Matrix4 allRotationMatrices = Matrix4::createRotationYMatrix(-Camera::getInstance().getYRotation()) *
+                                        Matrix4::createRotationXMatrix(-Camera::getInstance().getXRotation());
 
-    const Vector4 U(1.0f, 0.0f, 0.0f, 0.0f);
-    const Vector4 V(0.0f, 1.0f, 0.0f, 0.0f);
-    const Vector4 N(0.0f, 0.0f, 1.0f, 0.0f);
-
-    // @formatter:off
-    const Matrix4 uvnMatrix({
-        U.getX(), U.getY(), U.getZ(), 0,
-        V.getX(), V.getY(), V.getZ(), 0,
-        N.getX(), N.getY(), N.getZ(), 0,
-        0, 0, 0, 1
-    });
-    // @formatter:on
-
-    // @formatter:off
-    const Matrix4 cameraMatrix = uvnMatrix * Matrix4::createTranslationMatrix(-cameraPos.getX(), -cameraPos.getY(), -cameraPos.getZ());
-
-    const auto finalMatrix = matrixProjection * cameraMatrix * Matrix4::createTranslationMatrix(0, 0, NEAR_PLANE_Z * 5);
+    const Matrix4 finalMatrix = Camera::getInstance().getProjectionMatrix() * allTranslationMatrices * allRotationMatrices;
 
     const GLint projection = glGetUniformLocation(ShaderManager::getProgramId(), "projection");
     if (projection == -1)
@@ -87,7 +54,7 @@ void render(GLFWwindow* window, const double& now, double& lastRenderTime, unsig
     glfwPollEvents();
 }
 
-int main(const int argc, char** argv)
+static void handleDebugMode(const int argc, char **argv)
 {
     for (int i = 0; i < argc; ++i)
     {
@@ -96,6 +63,11 @@ int main(const int argc, char** argv)
             Logger::setDebug(true);
         }
     }
+}
+
+int main(const int argc, char** argv)
+{
+    handleDebugMode(argc, argv);
 
     // Initialize the library
     if (!glfwInit())
@@ -140,14 +112,13 @@ int main(const int argc, char** argv)
     BufferManager::init();
 
     BodyPart torso = BodyPart(Vector4(0.0f, 0.0f, 0.0f));
-    // BodyPart torso2 = BodyPart(Vector4(0.4f, 0.0f, 50.0f));
+    BodyPart torso2 = BodyPart(Vector4(0.5f, 0.5f, 0.5f));
 
     glfwSetKeyCallback(window, key_callback);
 
     double lastRenderTime = glfwGetTime();
     double lastFpsCountTime = glfwGetTime();
     unsigned int frameCount = 0;
-    double angle1 = 0;
 
     // Loop until the user closes the window
     while (!glfwWindowShouldClose(window))
@@ -157,76 +128,89 @@ int main(const int argc, char** argv)
         // Limit the frame rate ti FPS_LIMIT
         if ((now - lastRenderTime) >= 1.0 / FPS_LIMIT)
         {
+            // Rotate when pressing X
+            if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
+            {
+                Vector4 torsoDir = torso.getDir();
+                torsoDir.setX(torsoDir.getX() + 0.01f);
+                torso.setDir(torsoDir);
+            }
+
+            // Rotate when pressing Y
+            if (glfwGetKey(window, GLFW_KEY_Y) == GLFW_PRESS)
+            {
+                Vector4 torsoDir = torso.getDir();
+                torsoDir.setY(torsoDir.getY() + 0.01f);
+                torso.setDir(torsoDir);
+            }
+
+            // Rotate when pressing Z
+            if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS)
+            {
+                Vector4 torsoDir = torso.getDir();
+                torsoDir.setZ(torsoDir.getZ() + 0.01f);
+                torso.setDir(torsoDir);
+            }
+
+            // Move the camera forward
+            if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+            {
+                Camera::getInstance().updateCameraPos(FORWARD);
+            }
+
+            // Move the camera backward
+            if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+            {
+                Camera::getInstance().updateCameraPos(BACKWARD);
+            }
+
+            // Move the camera to the right
             if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        {
-            // torso.setPosition(torso.getPosition() + Vector4(0.00001f, 0.0f, 0.0f, 0.0f));
-            cameraPos.setX(cameraPos.getX() + 0.01f);
-        }
-        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        {
-            // torso.setPosition(torso.getPosition() - Vector4(0.00001f, 0.0f, 0.0f, 0.0f));
-            cameraPos.setX(cameraPos.getX() - 0.01f);
-        }
-        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        {
-            // torso.setPosition(torso.getPosition() + Vector4(0.0f, 0.00001f, 0.0f, 0.0f));
-            cameraPos.setY(cameraPos.getY() + 0.01f);
-        }
-        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        {
-            // torso.setPosition(torso.getPosition() - Vector4(0.0f, 0.00001f, 0.0f, 0.0f));
-            cameraPos.setY(cameraPos.getY() - 0.01f);
-        }
+            {
+                Camera::getInstance().updateCameraPos(RIGHT);
+            }
 
-        // Rotate when pressing X
-        if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
-        {
-            Vector4 torsoDir = torso.getDir();
-            torsoDir.setX(torsoDir.getX() + 0.01f);
-            torso.setDir(torsoDir);
-        }
+            // Move the camera to the left
+            if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+            {
+                Camera::getInstance().updateCameraPos(LEFT);
+            }
 
-        // Rotate when pressing Y
-        if (glfwGetKey(window, GLFW_KEY_Y) == GLFW_PRESS)
-        {
-            Vector4 torsoDir = torso.getDir();
-            torsoDir.setY(torsoDir.getY() + 0.01f);
-            torso.setDir(torsoDir);
-        }
+            // Move the camera upward
+            if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+            {
+                Camera::getInstance().updateCameraPos(UP);
+            }
 
-        // Rotate when pressing Z
-        if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS)
-        {
-            Vector4 torsoDir = torso.getDir();
-            torsoDir.setZ(torsoDir.getZ() + 0.01f);
-            torso.setDir(torsoDir);
-        }
+            // Move the camera downward
+            if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+            {
+                Camera::getInstance().updateCameraPos(DOWN);
+            }
 
-        // Increase z-axis when pressing UP_ARROW
-        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-        {
-            // Vector4 torsoPos = torso.getPosition();
-            // torsoPos.setZ(torsoPos.getZ() + 0.0001f);
-            // torso.setPosition(torsoPos);
-            cameraPos.setZ(cameraPos.getZ() + 0.01f);
-        }
+            if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+            {
+                Camera::getInstance().setXRotation(UP);
+            }
 
-        // Decrease z-axis when pressing DOWN_ARROW
-        if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-        {
-            // Vector4 torsoPos = torso.getPosition();
-            // torsoPos.setZ(torsoPos.getZ() - 0.0001f);
-            // torso.setPosition(torsoPos);
-            cameraPos.setZ(cameraPos.getZ() - 0.01f);
-        }
+            if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+            {
+                Camera::getInstance().setXRotation(DOWN);
+            }
 
-        if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-        {
-            cameraPos = Matrix4::createRotationYMatrix(-10 * std::numbers::pi / 180) * cameraPos;
-        }
+            if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+            {
+                Camera::getInstance().setYRotation(RIGHT);
+            }
+
+            if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+            {
+                Camera::getInstance().setYRotation(LEFT);
+            }
 
             render(window, now, lastRenderTime, frameCount);
         }
+
         if (now - lastFpsCountTime > 1.0)
         {
             Logger::info("main.cpp::main(): FPS: %f", frameCount / (now - lastFpsCountTime));
