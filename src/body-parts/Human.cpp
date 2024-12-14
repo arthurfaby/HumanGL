db28854cd6d0c4ef4522c4f21684c44cc2bc3721
Arthur Faby <arthurfaby1@gmail.com>
2024-12-14 15:20:21 +0100

    feat(human): put good values for bodyparts of human

--- src/body-parts/Human.cpp
+++ src/body-parts/Human.cpp
@@ -1,4 +1,7 @@
 #include "Human.hpp"
+#include "BodyPartDefines.hpp"
+#include "HumanDefines.hpp"
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Constructors
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -207,10 +210,12 @@ void Human::_initBodyParts()
 void Human::_initHead() const
 {
     if (!_head) return;
-    _head->scale(0.1f, 0.1f, 0.1f);
-    _head->setPivotPoint(Vector4(0.0f, -0.05f, 0.0f, 1.0f));
-    _head->translate(0.0f, 0.25f, 0.0f);
-    _head->setDefaultTranslate(0.0f, 0.25f, 0.0f);
+    _head->scale(HEAD_SCALE_X, HEAD_SCALE_Y, HEAD_SCALE_Z);
+    _head->setOwnRelativeShift(0, HEAD_SCALE_Y / 2, 0);
+    _head->setParentRelativeShift(0, TORSO_SCALE_Y / 2, 0);
+
+    _head->setPivotPoint(Vector4(0.0f, -HEAD_SCALE_Y / 2, 0.0f, 1.0f));
+
     _head->setColor(HEAD_COLOR);
 }
 
@@ -220,7 +225,7 @@ void Human::_initHead() const
 void Human::_initTorso() const
 {
     if (!_torso) return;
-    _torso->scale(0.2f, 0.4f, 0.1f);
+    _torso->scale(TORSO_SCALE_X, TORSO_SCALE_Y, TORSO_SCALE_Z);
     _torso->setColor(TORSO_COLOR);
 }
 
@@ -230,10 +235,12 @@ void Human::_initTorso() const
 void Human::_initRightArm() const
 {
     if (!_rightArm) return;
-    _rightArm->scale(0.3f, 0.04f, 0.04f);
-    _rightArm->translate(-0.2f, 0.13f, 0.0f);
-    _rightArm->setDefaultTranslate(-0.2f, 0.13f, 0.0f);
-    _rightArm->setPivotPoint(Vector4(0.08f, 0, 0, 1));
+    _rightArm->scale(RIGHT_ARM_SCALE_X, RIGHT_ARM_SCALE_Y, RIGHT_ARM_SCALE_Z);
+    _rightArm->setOwnRelativeShift(-RIGHT_ARM_SCALE_X / 2, RIGHT_ARM_SCALE_Y / 2, 0);
+    _rightArm->setParentRelativeShift(-TORSO_SCALE_X / 2, TORSO_SCALE_Y / 2, 0);
+
+    _rightArm->setPivotPoint(Vector4(RIGHT_ARM_SCALE_X / 2, -RIGHT_ARM_SCALE_Y / 2, 0, 1));
+
     _rightArm->setColor(RIGHT_ARM_COLOR);
 }
 
@@ -243,10 +250,11 @@ void Human::_initRightArm() const
 void Human::_initRightLowerArm() const
 {
     if (!_rightLowerArm) return;
-    _rightLowerArm->scale(0.3f, 0.025f, 0.025f);
-    _rightLowerArm->setPivotPoint(Vector4(0.10f, 0, 0, 1));
-    _rightLowerArm->translate(-0.2f, 0, 0);
-    _rightLowerArm->setDefaultTranslate(-0.2f, 0, 0);
+    _rightLowerArm->scale(RIGHT_LOWER_ARM_SCALE_X, RIGHT_LOWER_ARM_SCALE_Y, RIGHT_LOWER_ARM_SCALE_Z);
+    _rightLowerArm->setOwnRelativeShift(-RIGHT_LOWER_ARM_SCALE_X / 2, 0, 0);
+    _rightLowerArm->setParentRelativeShift(-RIGHT_ARM_SCALE_X / 2, 0, 0);
+
+    _rightLowerArm->setPivotPoint(Vector4(RIGHT_LOWER_ARM_SCALE_X / 2, 0, 0, 1));
     _rightLowerArm->setColor(RIGHT_LOWER_ARM_COLOR);
 }
 
@@ -256,10 +264,12 @@ void Human::_initRightLowerArm() const
 void Human::_initLeftArm() const
 {
     if (!_leftArm) return;
-    _leftArm->scale(0.3f, 0.05f, 0.05f);
-    _leftArm->setPivotPoint(Vector4(-0.08f, 0, 0, 1));
-    _leftArm->translate(0.2f, 0.13f, 0.0f);
-    _leftArm->setDefaultTranslate(0.2f, 0.13f, 0.0f);
+    _leftArm->scale(-LEFT_ARM_SCALE_X, LEFT_ARM_SCALE_Y, LEFT_ARM_SCALE_Z);
+    _leftArm->setOwnRelativeShift(LEFT_ARM_SCALE_X / 2, LEFT_ARM_SCALE_Y / 2, 0);
+    _leftArm->setParentRelativeShift(TORSO_SCALE_X / 2, TORSO_SCALE_Y / 2, 0);
+
+    _leftArm->setPivotPoint(Vector4(-LEFT_ARM_SCALE_X / 2, -LEFT_ARM_SCALE_Y / 2, 0, 1));
+
     _leftArm->setColor(LEFT_ARM_COLOR);
 }
 
@@ -269,10 +279,11 @@ void Human::_initLeftArm() const
 void Human::_initLeftLowerArm() const
 {
     if (!_leftLowerArm) return;
-    _leftLowerArm->scale(0.3f, 0.035f, 0.035f);
-    _leftLowerArm->setPivotPoint(Vector4(-0.10f, 0, 0, 1));
-    _leftLowerArm->translate(0.2f, 0, 0);
-    _leftLowerArm->setDefaultTranslate(0.2f, 0, 0);
+    _leftLowerArm->scale(-LEFT_LOWER_ARM_SCALE_X, LEFT_LOWER_ARM_SCALE_Y, LEFT_LOWER_ARM_SCALE_Z);
+    _leftLowerArm->setOwnRelativeShift(LEFT_LOWER_ARM_SCALE_X / 2, 0, 0);
+    _leftLowerArm->setParentRelativeShift(LEFT_ARM_SCALE_X / 2, 0, 0);
+
+    _leftLowerArm->setPivotPoint(Vector4(-LEFT_LOWER_ARM_SCALE_X / 2, 0, 0, 1));
     _leftLowerArm->setColor(LEFT_LOWER_ARM_COLOR);
 }
 
@@ -282,11 +293,13 @@ void Human::_initLeftLowerArm() const
 void Human::_initRightLeg() const
 {
     if (!_rightLeg) return;
-    _rightLeg->scale(0.05f, 0.2f, 0.05f);
-    _rightLeg->setPivotPoint(Vector4(0, 0.1f, 0, 1));
-    _rightLeg->translate(-0.05f, -0.3f, 0.0f);
-    _rightLeg->setDefaultTranslate(-0.05f, -0.3f, 0.0f);
-    _rightLeg->setColor(LEFT_LEG_COLOR);
+    _rightLeg->scale(RIGHT_LEG_SCALE_X, RIGHT_LEG_SCALE_Y, RIGHT_LEG_SCALE_Z);
+    _rightLeg->setOwnRelativeShift(0, -RIGHT_LEG_SCALE_Y / 2, 0);
+    _rightLeg->setParentRelativeShift(-TORSO_SCALE_X / 2, -TORSO_SCALE_Y / 2, 0);
+
+    _rightLeg->setPivotPoint(Vector4(0, RIGHT_LEG_SCALE_Y / 2, 0, 1));
+
+    _rightLeg->setColor(RIGHT_LEG_COLOR);
 }
 
 /**
@@ -295,11 +308,13 @@ void Human::_initRightLeg() const
 void Human::_initRightLowerLeg() const
 {
     if (!_rightLowerLeg) return;
-    _rightLowerLeg->scale(0.035f, 0.2f, 0.035f);
-    _rightLowerLeg->setPivotPoint(Vector4(0, 0.1f, 0, 1));
-    _rightLowerLeg->translate(0.0f, -0.2f, 0.0f);
-    _rightLowerLeg->setDefaultTranslate(0.0f, -0.2f, 0.0f);
-    _rightLowerLeg->setColor(LEFT_LOWER_LEG_COLOR);
+    _rightLowerLeg->scale(RIGHT_LOWER_LEG_SCALE_X, RIGHT_LOWER_LEG_SCALE_Y, RIGHT_LOWER_LEG_SCALE_Z);
+    _rightLowerLeg->setOwnRelativeShift(0, -RIGHT_LOWER_LEG_SCALE_Y / 2, 0);
+    _rightLowerLeg->setParentRelativeShift(0, -RIGHT_LEG_SCALE_Y / 2, 0);
+
+    _rightLowerLeg->setPivotPoint(Vector4(0, RIGHT_LOWER_LEG_SCALE_Y / 2, 0, 1));
+
+    _rightLowerLeg->setColor(RIGHT_LOWER_LEG_COLOR);
 }
 
 /**
@@ -308,10 +323,12 @@ void Human::_initRightLowerLeg() const
 void Human::_initLeftLeg() const
 {
     if (!_leftLeg) return;
-    _leftLeg->scale(0.05f, 0.2f, 0.05f);
-    _leftLeg->setPivotPoint(Vector4(0, 0.1f, 0, 1));
-    _leftLeg->translate(0.05f, -0.3f, 0.0f);
-    _leftLeg->setDefaultTranslate(0.05f, -0.3f, 0.0f);
+    _leftLeg->scale(LEFT_LEG_SCALE_X, LEFT_LEG_SCALE_Y, LEFT_LEG_SCALE_Z);
+    _leftLeg->setOwnRelativeShift(0, -LEFT_LEG_SCALE_Y / 2, 0);
+    _leftLeg->setParentRelativeShift(TORSO_SCALE_X / 2, -TORSO_SCALE_Y / 2, 0);
+
+    _leftLeg->setPivotPoint(Vector4(0, LEFT_LEG_SCALE_Y / 2, 0, 1));
+
     _leftLeg->setColor(LEFT_LEG_COLOR);
 }
 
@@ -321,10 +338,12 @@ void Human::_initLeftLeg() const
 void Human::_initLeftLowerLeg() const
 {
     if (!_leftLowerLeg) return;
-    _leftLowerLeg->scale(0.035f, 0.2f, 0.035f);
-    _leftLowerLeg->setPivotPoint(Vector4(0, 0.1f, 0, 1));
-    _leftLowerLeg->translate(0.0f, -0.2f, 0.0f);
-    _leftLowerLeg->setDefaultTranslate(0.0f, -0.2f, 0.0f);
+    _leftLowerLeg->scale(LEFT_LOWER_LEG_SCALE_X, LEFT_LOWER_LEG_SCALE_Y, LEFT_LOWER_LEG_SCALE_Z);
+    _leftLowerLeg->setOwnRelativeShift(0, -LEFT_LOWER_LEG_SCALE_Y / 2, 0);
+    _leftLowerLeg->setParentRelativeShift(0, -LEFT_LEG_SCALE_Y / 2, 0);
+
+    _leftLowerLeg->setPivotPoint(Vector4(0, LEFT_LOWER_LEG_SCALE_Y / 2, 0, 1));
+
     _leftLowerLeg->setColor(LEFT_LOWER_LEG_COLOR);
 }
 
