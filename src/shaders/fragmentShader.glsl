8215b401fae23d0d14a225c532a8c138f48342e0
Arthur Faby <arthurfaby1@gmail.com>
2024-12-16 09:14:50 +0100

    MR: simplify fragment shader

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -5,5 +5,5 @@ in vec3 fragmentColor;  // Color from the geometry shader
 out vec3 finalColor;    // Final color of the fragment
 
 void main() {
-    finalColor = vec3(fragmentColor.r, fragmentColor.g, fragmentColor.b);
+    finalColor = fragmentColor;
 }
