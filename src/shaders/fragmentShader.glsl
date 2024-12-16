d200e16197026f6f7b03e8e4662ef91a9d73d079
Arthur Faby <arthurfaby1@gmail.com>
2024-12-16 09:13:12 +0100

    MR: add tabs to glsl shaders

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -1,8 +1,8 @@
 #version 450 core
 
-in vec3 fragmentColor;// Color from the geometry shader
+in vec3 fragmentColor;  // Color from the geometry shader
 
-out vec3 finalColor;// Final color of the fragment
+out vec3 finalColor;    // Final color of the fragment
 
 void main() {
     finalColor = vec3(fragmentColor.r, fragmentColor.g, fragmentColor.b);
